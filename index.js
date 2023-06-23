02b6a7a8c5aa9cdaa9d7ce59fbcc47f667c9d1e7
drios2023-ga <drios111-general@yahoo.com>
2023-06-23 14:47:23 -0500

    completed demo

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 //require dotenv here
+require('dotenv').config();
 const express = require("express");
 const app = express();
 const path = require("path");
 require("ejs");
 const port = 8080;
+
+
+const { storage } = require('./storage/storage');
+const multer = require('multer');
+const upload = multer({ storage });
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,7 +23,12 @@ app.get("", (req, res) => {
 });
 
 // code goes here
-
+//we used upload.single to tell "multer" to upload
+// only single image 
+app.post('/upload', upload.single('image'), (req, res) => {
+  console.log(req.file);
+  res.send('Done');
+});
 
 
 app.listen(port, () => {
